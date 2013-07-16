2463d4b60e4ddf833024e590c0abaa251a19e095
Kyle Shannon <kyle@pobox.com>
2013-07-16 11:40:52 -0600

    update licensing, move from public domain (fuzzy) to MIT/X

--- src/showind.py
+++ src/showind.py
@@ -1,13 +1,23 @@
-#!/usr/bin/python
-#******************************************************************************
+#!/usr/bin/env python
+###############################################################################
+# $Id$
 #
-#  Project:  showind.py
-#  Purpose:  Access to hobo wind data
-#  Author:   Kyle Shannon <ksshannon@gmail.com>
+#  Project:  showind
+#  Purpose:  Display and subset wind data
+#  Author:   Kyle Shannon <kyle@pobox.com>
 #
-#******************************************************************************
-#  The author disclaims copyright to this source code and places it into the
-#  Public Domain.
+###############################################################################
+#  Copyright (c) 2013, Kyle Shannon <kyle@pobox.com>
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included
+#  in all copies or substantial portions of the Software.
 #
 #  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 #  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -16,7 +26,7 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
-#******************************************************************************
+###############################################################################
 
 from collections import namedtuple
 import datetime
